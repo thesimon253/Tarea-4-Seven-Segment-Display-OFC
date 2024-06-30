ce32d78b04e0728980f2a2d2ddae18a3984c5252
Simon Peralta <161258376+thesimon253@users.noreply.github.com>
2024-06-29 23:49:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://thesimon253.github.io/Tarea-4-Seven-Segment-Display-OFC",
 })
